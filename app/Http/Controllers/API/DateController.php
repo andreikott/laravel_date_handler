ea486a578abc4a8e5dd20a0fd4ae938f420a4f5c
Andrei Kot <kot240@gmail.com>
2020-10-14 21:43:04 +0300

    API method

---
+++ app/Http/Controllers/API/DateController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
+
+class DateController extends Controller
+{
+    /**
+     * Holidays.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function holidays(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'date' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => collect($validator->errors())->collapse()->first()], 400);
+        }
+
+        $date = Carbon::parse($request->input('date'));
+
+        $holidays = collect(config('holidays'))->map(function ($holiday) use ($date) {
+            $holiday['start'] = Carbon::parse($holiday['start'])->setYear($date->format('Y'))->timestamp;
+
+            $holidayEnd = $holiday['start'];
+            if (isset($holiday['end']) AND ! empty($holiday['end'])) {
+                $holidayEnd = $holiday['end'];
+            }
+            $holiday['end'] = Carbon::parse($holidayEnd)->setYear($date->format('Y'))->timestamp;
+
+            return $holiday;
+        });
+
+        $filteredHolidays = $holidays->where('start', '>=', $date->timestamp)
+            ->where('end', '<=', $date->timestamp)->pluck('name');
+
+        return response()->json(['holidays' => $filteredHolidays]);
+    }
+}
---
+++ config/holidays.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kot
+ * Date: 10/14/20
+ * Time: 4:26 AM
+ */
+
+return [
+    [
+        'name' => '1st of January',
+        'start' => 1577836800,
+        'end' => null
+    ],
+    [
+        'name' => '7th of January',
+        'start' => 1578355200,
+        'end' => null
+    ],
+    [
+        'name' => 'From 1st of May till 7th of May',
+        'start' => 1588291200,
+        'end' => 1588809600
+    ],
+    [
+        'name' => 'Monday of the 3rd week of January',
+        'start' => 1578873600,
+        'end' => null
+    ],
+    [
+        'name' => 'Monday of the last week of March',
+        'start' => 1585526400,
+        'end' => null
+    ],
+    [
+        'name' => 'Thursday of the 4th week of November',
+        'start' => 1606348800,
+        'end' => null
+    ]
+];
